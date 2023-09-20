e237ca40a64628e9230b0fb63cdcd96402d6ba94
daniel <chididaniel1998@gmail.com>
2023-09-20 05:48:33 -0700

    send

--- src/body.jsx
+++ src/body.jsx
@@ -1,9 +1,11 @@
 import Brush from "./images/brush.jpg";
-import Brush2 from "./images/brush2.jpg";
+import Brush2 from "./images/painting.jpg";
 import newyork from "./images/newyork.jpg";
 import turtel from "./images/turtel.jpg";
-import Card from "react-bootstrap/Card";
-import Image from "react-bootstrap/Image";
+import nature from "./images/nature.jpg";
+import nature2 from "./images/nature2.jpg";
+import city from "./images/city.jpg";
+import flower from "./images/flower.jpg";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
@@ -11,8 +13,7 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import { Sortable } from "./sortable";
-import Form from 'react-bootstrap/Form';
-
+import Form from "react-bootstrap/Form";
 
 import { DndContext, closestCenter } from "@dnd-kit/core";
 
@@ -31,69 +32,71 @@ const Body = () => {
     {
       title: Brush,
       id: crypto.randomUUID(),
-      tag: "art",
+      tag: "ART",
     },
-    { title: Brush2, id: crypto.randomUUID(), tag: "art" },
+    { title: Brush2, id: crypto.randomUUID(), tag: "ART" },
     {
       title: newyork,
       id: crypto.randomUUID(),
-      tag: "city",
+      tag: "CITY",
     },
-    { title: turtel, id: crypto.randomUUID(), tag: "animal" },
+    { title: turtel, id: crypto.randomUUID(), tag: "ANIMAL" },
+    { title: city, id: crypto.randomUUID(), tag: "CITY" },
+
+    { title: flower, id: crypto.randomUUID(), tag: "FLOWER" },
+    { title: nature, id: crypto.randomUUID(), tag: "NATURE" },
+    { title: nature2, id: crypto.randomUUID(), tag: "NATURE" },
   ]);
 
- const [filters,setFilters] = useState("")
-
- 
+  const [filters, setFilters] = useState("");
 
   const handleDrag = (event) => {
-    const {active, over} = event;
-   
-    setList(users => {
-        const oldIndex = users.findIndex(user => user.id === active.id)
-        const newIndex = users.findIndex(user => user.id === over.id)
-        return arrayMove(list, oldIndex,newIndex)
-    })
+    const { active, over } = event;
+
+    setList((users) => {
+      const oldIndex = users.findIndex((user) => user.id === active.id);
+      const newIndex = users.findIndex((user) => user.id === over.id);
+      return arrayMove(list, oldIndex, newIndex);
+    });
   };
 
   const handleFilter = (e) => {
-   
-     if (filters === ""){
-        return list
-     }
-    return  list.filter( fil => fil.tag === filters);
-     
- 
-  }
-
-
+    if (filters === "") {
+      return list;
+    }
+    return list.filter((fil) => fil.tag === filters.toUpperCase());
+  };
 
   return (
     <>
-    <Form onSubmit={(e)=>e.preventDefault()}>
-          <Form.Control onChange={ e=> setFilters(e.target.value)} value={filters} type="text" placeholder="Normal text"/>
-    </Form>
+      <Form onSubmit={(e) => e.preventDefault()}>
+        <Form.Control
+          class="text-uppercase"
+          onChange={(e) => setFilters(e.target.value)}
+          value={filters}
+          type="text"
+          placeholder="Normal text"
+        />
+      </Form>
 
-   
       <DndContext collisionDetection={closestCenter} onDragEnd={handleDrag}>
         <SortableContext items={list} strategy={rectSortingStrategy}>
           <Container>
             <Row>
-             
-                { handleFilter().map((lis) => (
-                <Sortable
-                  key={lis.id}
-                  id={lis.id}
-                  title={lis.title}
-                  tag={lis.tag}
-                />
-              ))}
-              
+              {handleFilter().length > 0
+                ? handleFilter().map((lis) => (
+                    <Sortable
+                      key={lis.id}
+                      id={lis.id}
+                      title={lis.title}
+                      tag={lis.tag}
+                    />
+                  ))
+                : "not found"}
             </Row>
           </Container>
         </SortableContext>
       </DndContext>
-      
     </>
   );
 };
--- src/sortable.jsx
+++ src/sortable.jsx
@@ -8,33 +8,33 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import { useState } from "react";
-import './index.css'
+import "./index.css";
 export function Sortable(props) {
-  const { attributes, listeners, setNodeRef, transform, transition} =
+  const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: props.id });
-
-    
 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
-    
-    
   };
-
- 
 
   return (
     <>
-    
-    <Col xs={6} md={4}>
-      <div className="map" ref={setNodeRef} style={style} {...attributes}   {...listeners}>
-        <Card>
-          <Card.Img src={props.title} />
+      <Col xs={6} md={4}>
+        <div
+          className="map"
+          ref={setNodeRef}
+          style={style}
+          {...attributes}
+          {...listeners}
+        >
+          <Card>
+            <Card.Img src={props.title} />
 
-          <Card.Footer className="ps-0">{props.tag}</Card.Footer>
-        </Card>
-      </div>
-    </Col>
- </> );
+            <Card.Footer className="ps-0">{props.tag}</Card.Footer>
+          </Card>
+        </div>
+      </Col>
+    </>
+  );
 }
